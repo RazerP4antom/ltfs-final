37d52653be38ae917db192830a135a8a5990b95e
RazerP4antom <mirjisarthak3@gmail.com>
2023-05-11 16:46:33 +0530

    Ready for deployment

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, redirect, render_template, url_for
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from werkzeug.utils import secure_filename
+import os
+from wtforms.validators import InputRequired
+import pdftables_api
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'supersecretkey'
+app.config['UPLOAD_FOLDER'] = 'static/files'
+c = pdftables_api.Client('rvaootxkiksu')
+
+class UploadFileForm(FlaskForm):
+    file = FileField("File", validators=[InputRequired()])
+    submit = SubmitField("Upload File")
+
+@app.route('/', methods=['GET',"POST"])
+@app.route('/home', methods=['GET',"POST"])
+def home():
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        file = form.file.data # First grab the file
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
+        input_file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'], filename)
+        output_file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'], f'{filename}.xlsx')
+        c = pdftables_api.Client('rvaootxkiksu')
+        c.xlsx(input_file_path, output_file_path)
+        
+        return 'File has been uploaded and converted to Excel. Please check your local directory'
+    return render_template('index.html', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
